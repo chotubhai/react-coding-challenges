e60d61a731adfbd4e4dcc0d7e66f83dabc774f18
Ritik Singh <mr.ritik99@gmail.com>
2021-05-18 02:02:50 +0530

    update gitignore

--- spootify/src/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  api: {
-    baseUrl: 'https://api.spotify.com/v1',
-    authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '40e78a830b7d4035a8a840a9def4305f',
-    clientSecret: '17aca3b6e6b84c798b5e7652408dd1ce'
-  }
-}
